ce9ace356921971bf8a3a50fb0819b8e627bd17a
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-05-19 14:33:11 -0400

    Create xsplugins-xershadescorner.php

---
+++ xsplugins-xershadescorner.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: XerShade's Plugns - XerShade's Corner
+Plugin URI: https://github.com/XerShade/XSPlugins.XerShadesCorner
+Description: A wordpress plugin that provides website specific code changes for 'xershade.ca'.
+Author: XerShade
+Author URI: https://www.xershade.ca/
+Version: 1.0.0
+*/
