46fb9b31022e3caeef2b4fdac245610b8d48c577
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-05-25 00:06:18 -0400

    Moved style overrides to plugin.

--- xsplugins-xershadescorner.php
+++ xsplugins-xershadescorner.php
@@ -2,8 +2,13 @@
 /*
 Plugin Name: XerShade's Plugins - XerShade's Corner
 Plugin URI: https://github.com/XerShade/XSPlugins.XerShadesCorner
-Description: A wordpress plugin that provides website specific code changes for 'xershade.ca'.
+Description: A WordPress plugin that provides website specific code changes for 'xershade.ca'.
 Author: XerShade
 Author URI: https://www.xershade.ca/
 Version: 1.0.0
 */
+
+add_action( 'wp_enqueue_scripts', 'xsc_register_stylesheets', 100 );
+function xsc_register_stylesheets() {
+	wp_enqueue_style( 'xershades.corner.overrides', plugins_url( 'css/xershades.corner.overrides.css', __FILE__ ) );
+}
